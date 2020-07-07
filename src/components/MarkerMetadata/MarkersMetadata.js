9827aaa8ccefd9da6013e34c30af9f9835afd050
yingfeng-iu <yingfeng@iu.edu>
2020-07-07 18:44:10 -0400

    AMP-627: - fix current time indicator - comment out unused code

--- src/components/MarkerMetadata/MarkersMetadata.js
+++ src/components/MarkerMetadata/MarkersMetadata.js
@@ -101,16 +101,19 @@ export default function MarkersMetadata(props) {
             height: '30px !important',
             paddingTop: 0,
             paddingBottom: 0,
+            padding: '10px 10px 0 0',
           },
           headerSytle: {
             height: '30px !important',
             paddingTop: 0,
             paddingBottom: 0,
+            padding: '10px 10px 0 0',
           },
           rowStyle: {
             height: '30px !important',
             paddingTop: 0,
             paddingBottom: 0,
+            padding: '10px 10px 0 0',
           },
       }}
       actions={[
